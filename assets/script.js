12564df9e0b7e99ec815e5616b59998051c28dfd
Titidafi Tv <Titidafi Tv>
2017-08-06 04:27:25 +0200

    Get beautified

--- assets/script.js
+++ assets/script.js
@@ -8,13 +8,23 @@ $(document).ready(function () {
     $('h1').slideUp(0);
     $('h1').delay(1000).slideDown("slow");
     $('hr').delay(1400).show("slow");
-var actualTimer = 1200;
+    var actualTimer = 1200;
 
     $('#element1').delay(1400).fadeIn("slow");
     $('#element2').delay(1800).fadeIn("slow");
     $('#element3').delay(2200).fadeIn("slow");
     $('#element4').delay(2600).fadeIn("slow");
-    
+    $('body').scroll(function () {
 
+
+        $("#map").animate({
+            opacity: 0.25,
+            left: '-25px',
+        }, 200, function () {
+            // Animation complete.
+        });
+
+
+
+    });
 });
-
